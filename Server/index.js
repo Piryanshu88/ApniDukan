65cdc6cdb6b38989789d06e89155c41003045096
Piryanshu88 <deepubisht2004@gmail.com>
2023-03-01 03:54:52 +0530

    addding folder structure

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 app.get("/", (req, res) => {
-  res.send("This is the home page ");
+  res.send("This is the home page of HM clone ");
 });
 
 app.listen(process.env.port, () => {
