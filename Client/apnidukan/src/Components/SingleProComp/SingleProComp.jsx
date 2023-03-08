2485c805299dc479a477b804e78848db5a7da7b6
Piryanshu88 <deepubisht2004@gmail.com>
2023-03-08 13:26:30 +0530

    adding single page upper section

--- Client/apnidukan/src/Components/SingleProComp/SingleProComp.jsx
+++ Client/apnidukan/src/Components/SingleProComp/SingleProComp.jsx
@@ -1,7 +1,8 @@
 import { Image, Text, useDisclosure } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { GrStar } from "react-icons/gr";
 import {
   getDataById,
   getDataErrorById,
@@ -10,35 +11,79 @@ import {
 import styles from "./SingleProduct.module.css";
 import { ProDucts } from "../../constants";
 export const SingleProComp = () => {
-  const { id } = useParams();
-  const [data, setData] = useState({});
+  const { id, category } = useParams();
+  const navigate = useNavigate();
+  const [articleCode, setArticleCode] = useState(id);
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
   const { products, isLoading, isError } = useSelector(
     (store) => store.singleDataReducer
   );
   useEffect(() => {
-    dispatch(getDataById(id))
+    setLoading(true);
+    dispatch(getDataById(articleCode))
       .then((re) => {
         dispatch(getDataSuccessById(re.data.product));
       })
       .catch((err) => dispatch(getDataErrorById()));
-    console.log("products", products);
-  }, [id]);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    console.log(products);
+    products?.articlesList?.map((el, i) => {
+      console.log(el.galleryDetails[0]?.baseUrl);
+    });
+  }, [id, articleCode]);
 
+  const handleArticle = (e) => {
+    setArticleCode(e);
+    navigate(`/singleproduct/${category}/${e}`);
+  };
   if (isError) {
     return <Text>error</Text>;
+  }
+  if (loading) {
+    return <Text>Loading...</Text>;
   }
 
   return (
     <div className={styles.single_page_box}>
       <div className={styles.singlepage}>
         <div className={styles.img_box}>
-          {/* <Image src={data?.image[0]?.src} alt={data?.image[0]?.alt} />
-          <Image
-            src={data?.image[0]?.dataAltImage}
-            alt={data?.image[0]?.dataAltText}
-          /> */}
-          <Text>{products?.code}</Text>
+          <Image src={products?.articlesList[0].galleryDetails[0]?.baseUrl} />
+          <Image src={products?.articlesList[0].galleryDetails[1]?.baseUrl} />
+        </div>
+        <div className={styles.single_page_details}>
+          <Text>{products.name}</Text>
+          <Text>{`Rs. ${products.whitePrice.price}.00`}</Text>
+          <Text>{products.color.text}</Text>
+          <div className={styles.color_article}>
+            {products?.articlesList?.map((el, i) => {
+              return (
+                <Image
+                  src={el.galleryDetails[0]?.baseUrl}
+                  key={i}
+                  height="100px"
+                  objectFit={"contain"}
+                  padding="2px"
+                  border={
+                    articleCode == el.code
+                      ? "1px solid var(--text-color)"
+                      : null
+                  }
+                  onClick={() => handleArticle(el.code)}
+                />
+              );
+            })}
+          </div>
+          <div className={styles.review_box}>
+            <Text>REVIEWS (5)</Text>
+            <GrStar />
+            <GrStar />
+            <GrStar />
+            <GrStar />
+            <GrStar />
+          </div>
         </div>
       </div>
     </div>
