fe9b578648d180ecaf3a2ea96243dad519b7f280
Piryanshu88 <deepubisht2004@gmail.com>
2023-03-15 00:23:55 +0530

    adding actiontype for cart

---
+++ Client/apnidukan/src/redux/cartReducer/actionTypes.ts
@@ -0,0 +1,7 @@
+export const GET_CART_REQ = "GET_CART_REQ";
+export const GET_CART_SUCCESS = "GET_CART_SUCCESS";
+export const GET_CART_ERROR = "GET_CART_ERROR";
+
+export const DEL_CART_REQ = "DEL_CART_REQ";
+export const DEL_CART_SUCCESS = "DEL_CART_SUCCESS";
+export const DEL_CART_ERROR = "DEL_CART_ERROR";
