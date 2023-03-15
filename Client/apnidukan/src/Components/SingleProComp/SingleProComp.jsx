96228f10108195a68221e8f217a1813318039513
Piryanshu88 <deepubisht2004@gmail.com>
2023-03-15 19:50:00 +0530

    adding skeleton in all the screen sizes

--- Client/apnidukan/src/Components/SingleProComp/SingleProComp.jsx
+++ Client/apnidukan/src/Components/SingleProComp/SingleProComp.jsx
@@ -3,6 +3,7 @@ import {
   Button,
   Image,
   Select,
+  Skeleton,
   Text,
   useDisclosure,
   useToast,
@@ -82,43 +83,53 @@ export const SingleProComp = () => {
     getData(category).then((re) => {
       setScrollData(re.data);
     });
-
+    setArticle(
+      products?.articlesList?.filter((el) => el?.code == articleCode)[0]
+    );
     setTimeout(() => {
-      setArticle(
-        products?.articlesList?.filter((el) => el?.code == articleCode)[0]
-      );
       setLoading(false);
     }, 4000);
     console.log(products);
-  }, [articleCode]);
+  }, [articleCode, isAuth]);
 
   if (isError) {
     return <Text>error</Text>;
-  }
-  if (loading) {
-    return <Text>Loading...</Text>;
   }
 
   return (
     <div className={styles.single_page_box}>
       <div className={styles.singlepage}>
         <div>
-          <div className={styles.img_box}>
-            <Image
-              src={
-                products?.articlesList?.filter(
-                  (el) => el?.code == articleCode
-                )[0]?.galleryDetails[0]?.baseUrl
-              }
-            />
-            <Image
-              src={
-                products?.articlesList?.filter(
-                  (el) => el?.code == articleCode
-                )[0]?.galleryDetails[1]?.baseUrl
-              }
-            />
-          </div>
+          {loading ? (
+            <div className={styles.img_box}>
+              <Skeleton
+                height={"500px"}
+                width="100%"
+                marginRight={"10px"}
+              ></Skeleton>
+              <Skeleton
+                height={"500px"}
+                width={{ base: "0%", sm: "100%" }}
+              ></Skeleton>
+            </div>
+          ) : (
+            <div className={styles.img_box}>
+              <Image
+                src={
+                  products?.articlesList?.filter(
+                    (el) => el?.code == articleCode
+                  )[0]?.galleryDetails[0]?.baseUrl
+                }
+              />
+              <Image
+                src={
+                  products?.articlesList?.filter(
+                    (el) => el?.code == articleCode
+                  )[0]?.galleryDetails[1]?.baseUrl
+                }
+              />
+            </div>
+          )}
 
           {/* only for mobile */}
           <div className={styles.single_page_details_mobile}>
@@ -195,65 +206,120 @@ export const SingleProComp = () => {
           </div>
         </div>
 
-        <div className={styles.single_page_details}>
-          <Text>{products?.name}</Text>
-          <Text>{`Rs. ${products?.whitePrice?.price}.00`}</Text>
-          <Text>{products?.color?.text}</Text>
-          <div className={styles.color_article}>
-            {products?.articlesList?.splice(0, 4).map((el, i) => {
-              return (
-                <Image
-                  src={el?.galleryDetails[0]?.baseUrl}
-                  key={i}
-                  height="100px"
-                  objectFit={"contain"}
-                  padding="2px"
-                  border={
-                    articleCode == el?.code
-                      ? "1px solid var(--text-color)"
-                      : null
-                  }
-                  onClick={() => ChangeActiveArticle(el.code)}
-                />
-              );
-            })}
+        {loading ? (
+          <Box className={styles.single_page_details} position="static">
+            <Skeleton
+              height={"20px"}
+              width="300px"
+              marginBottom={"10px"}
+            ></Skeleton>
+            <Skeleton
+              height={"55x"}
+              width="200px"
+              marginBottom={"10px"}
+            ></Skeleton>
+            <Skeleton
+              marginBottom={"10px"}
+              height={"10px"}
+              width="150px"
+            ></Skeleton>
+            <div className={styles.color_article}>
+              {[1, 1, 1, 1].map((el) => {
+                return <Skeleton height={"100px"} width="60px"></Skeleton>;
+              })}
+            </div>
+
+            <div className={styles.review_box}>
+              <Skeleton width={"200px"} height="30px"></Skeleton>
+            </div>
+
+            <div className={styles.size_box}>
+              <Skeleton
+                width={"100%"}
+                height="50px"
+                marginTop={"30px"}
+              ></Skeleton>
+            </div>
+
+            <div className={styles.add_to_cart_box}>
+              <Skeleton
+                width={"100%"}
+                height="45px"
+                marginTop={"30px"}
+              ></Skeleton>
+              <Skeleton
+                width={"50px"}
+                marginTop="10px"
+                height="10px"
+              ></Skeleton>
+              <Skeleton
+                marginTop="10px"
+                width={"100px"}
+                height="15px"
+              ></Skeleton>
+            </div>
+          </Box>
+        ) : (
+          <div className={styles.single_page_details}>
+            <Text>{products?.name}</Text>
+            <Text>{`Rs. ${products?.whitePrice?.price}.00`}</Text>
+            <Text>{products?.color?.text}</Text>
+            <div className={styles.color_article}>
+              {products?.articlesList?.splice(0, 4).map((el, i) => {
+                return (
+                  <Image
+                    src={el?.galleryDetails[0]?.baseUrl}
+                    key={i}
+                    height="100px"
+                    objectFit={"contain"}
+                    padding="2px"
+                    border={
+                      articleCode == el?.code
+                        ? "1px solid var(--text-color)"
+                        : null
+                    }
+                    onClick={() => ChangeActiveArticle(el.code)}
+                  />
+                );
+              })}
+            </div>
+            <div className={styles.review_box}>
+              <Text>REVIEWS (5)</Text>
+              <GrStar />
+              <GrStar />
+              <GrStar />
+              <GrStar />
+              <GrStar />
+            </div>
+            <div className={styles.size_box}>
+              <Select placeholder="Select Size" borderRadius={"0"}>
+                <option value="option1">S</option>
+                <option value="option2"> M</option>
+                <option value="option3"> L</option>
+              </Select>
+            </div>
+            <div className={styles.add_to_cart_box}>
+              <Button
+                borderRadius={"0"}
+                colorScheme="blackAlpha"
+                width={"100%"}
+                display="flex"
+                alignItems={"center"}
+                background="var(--text-color)"
+                gap={"10px"}
+                onClick={handleAddToCart}
+              >
+                <CiBag1 />
+                Add to Cart
+              </Button>
+            </div>
+            <Text display={"flex"} gap="10px" alignItems={"center"}>
+              <CiCircleInfo />
+              Standard delivery in 2-7 days
+            </Text>
+            <Text fontWeight={"500"}>Delivery and Payment</Text>
           </div>
-          <div className={styles.review_box}>
-            <Text>REVIEWS (5)</Text>
-            <GrStar />
-            <GrStar />
-            <GrStar />
-            <GrStar />
-            <GrStar />
-          </div>
-          <div className={styles.size_box}>
-            <Select placeholder="Select Size" borderRadius={"0"}>
-              <option value="option1">S</option>
-              <option value="option2"> M</option>
-              <option value="option3"> L</option>
-            </Select>
-          </div>
-          <div className={styles.add_to_cart_box}>
-            <Button
-              borderRadius={"0"}
-              colorScheme="blackAlpha"
-              width={"100%"}
-              display="flex"
-              alignItems={"center"}
-              background="var(--text-color)"
-              gap={"10px"}
-              onClick={handleAddToCart}
-            >
-              <CiBag1 />
-              Add to Cart
-            </Button>
-          </div>
-          <Text display={"flex"} gap="10px" alignItems={"center"}>
-            <CiCircleInfo />
-            Standard delivery in 2-7 days
-          </Text>
-          <Text fontWeight={"500"}>Delivery and Payment</Text>
-        </div>
+        )}
       </div>
 
       {/* mobile add to cart */}
