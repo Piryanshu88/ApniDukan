f746020ca303f29fd737e54ae6ecb5cd7d326cf4
Piryanshu88 <deepubisht2004@gmail.com>
2023-03-31 17:54:21 +0530

    page not found img :tada:

--- Client/apnidukan/src/Pages/AllRoutes.tsx
+++ Client/apnidukan/src/Pages/AllRoutes.tsx
@@ -1,3 +1,4 @@
+import { Image } from "@chakra-ui/react";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { CartPage } from "./Cart/CartPage";
@@ -17,6 +18,18 @@ export const AllRoutes = () => {
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/category/:category" element={<ProductPage />} />
       <Route path="/cart" element={<CartPage />} />
+      <Route
+        path="*"
+        element={
+          <Image
+            src="https://cdni.iconscout.com/illustration/premium/thumb/404-page-not-found-4190275-3468592.png"
+            margin={"auto"}
+            objectFit="contain"
+            marginTop={"20px"}
+            marginBottom={"20px"}
+          />
+        }
+      />
     </Routes>
   );
 };
