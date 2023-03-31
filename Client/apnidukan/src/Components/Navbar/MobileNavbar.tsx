617a4988d504c22fc08c7153a7eb2d3ed72a6504
Piryanshu88 <deepubisht2004@gmail.com>
2023-03-31 18:20:13 +0530

    adding navbar for mobiles :construction_worker:

--- Client/apnidukan/src/Components/Navbar/MobileNavbar.tsx
+++ Client/apnidukan/src/Components/Navbar/MobileNavbar.tsx
@@ -1,4 +1,11 @@
-import { Button, IconButton, useDisclosure } from "@chakra-ui/react";
+import {
+  Button,
+  IconButton,
+  Image,
+  Stack,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import React from "react";
 import styles from "./Navbar.module.css";
 import {
@@ -11,9 +18,16 @@ import {
   DrawerCloseButton,
 } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
+import { Link, useNavigate } from "react-router-dom";
 export const MobileNavbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef: any = React.useRef();
+  const navigate = useNavigate();
+  const handleClick = (route: string) => {
+    onClose();
+    navigate(`/category/${route}`);
+  };
+
   return (
     <div className={styles.mobile_nav}>
       <Button
@@ -33,9 +47,86 @@ export const MobileNavbar = () => {
         size="xs"
       >
         {/* <DrawerOverlay /> */}
-        <DrawerContent background={"var(--color-primary)"}>
+        <DrawerContent background={"var(--color-bg)"}>
           <DrawerCloseButton />
-          <DrawerBody></DrawerBody>
+          <DrawerBody>
+            <Image src="" />
+            <Stack
+              marginTop="40px"
+              fontSize={"xl"}
+              fontWeight="400"
+              alignItems={"center"}
+              gap="15px"
+            >
+              <Text
+                onClick={() => handleClick("ladies")}
+                cursor={"pointer"}
+                _hover={{ color: "var(--text-color)" }}
+              >
+                Ladies
+              </Text>
+
+              <Text
+                cursor={"pointer"}
+                _hover={{ color: "var(--text-color)" }}
+                onClick={() => handleClick("mens")}
+              >
+                Mens
+              </Text>
+
+              <Text
+                cursor={"pointer"}
+                _hover={{ color: "var(--text-color)" }}
+                onClick={() => handleClick("dividend")}
+              >
+                Dividend
+              </Text>
+
+              <Text
+                onClick={() => handleClick("sports")}
+                cursor={"pointer"}
+                _hover={{ color: "var(--text-color)" }}
+              >
+                Sports
+              </Text>
+
+              <Text
+                cursor={"pointer"}
+                _hover={{ color: "var(--text-color)" }}
+                onClick={() => handleClick("home")}
+              >
+                Home
+              </Text>
+
+              <Text
+                onClick={() => handleClick("kids")}
+                cursor={"pointer"}
+                _hover={{ color: "var(--text-color)" }}
+              >
+                Kids
+              </Text>
+
+              <Text
+                onClick={() => handleClick("sale")}
+                cursor={"pointer"}
+                _hover={{ color: "var(--text-color)" }}
+              >
+                Sale
+              </Text>
+
+              <Text cursor={"pointer"} _hover={{ color: "var(--text-color)" }}>
+                Sustainability
+              </Text>
+            </Stack>
+          </DrawerBody>
+          <DrawerFooter>
+            <Image
+              src="https://readme-typing-svg.demolab.com/?font=Fira+Code&size=14&duration=4000&pause=1000&color=444444&background=FFFFFF00&width=220&height=31&lines=Made+with+🧡+by+Piryanshu"
+              alt="Typing SVG"
+              // style={{ margin: "auto" }}
+              margin={"auto"}
+            />
+          </DrawerFooter>
         </DrawerContent>
       </Drawer>
     </div>
